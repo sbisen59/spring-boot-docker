44235e461d2986e310b359a05773f7bf4f6e214b
sbisen59@gmail.com <sbisen59@gmail.com>
2021-05-01 10:39:18 +0530

    ftp deployment

--- src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDockerApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Hello from FTP deployment World";
 	}
 
 	public static void main(String[] args) {
